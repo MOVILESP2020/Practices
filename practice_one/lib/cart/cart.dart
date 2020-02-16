05373cc8a1d682a41f881d907deab0f8898795e5
isaac <isaaccabrera31@gmail.com>
2020-02-16 12:40:55 -0600

    Cart page impl.

--- practice_one/lib/cart/cart.dart
+++ practice_one/lib/cart/cart.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:practice_one/cart/item_cart.dart';
+import 'package:practice_one/colors.dart';
+import 'package:practice_one/models/cart_repository.dart';
+import 'package:practice_one/models/product.dart';
+import 'package:practice_one/payment/payment.dart';
+import 'package:practice_one/profile.dart';
+import 'package:practice_one/utils/Formatter.dart';
+import 'package:practice_one/utils/themes.dart';
 
 class Cart extends StatefulWidget {
   final List<dynamic> productList;
   Cart({
     Key key,
-    @required this.productList,
+    this.productList,
   }) : super(key: key);
 
   @override
@@ -12,8 +20,115 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+
+  double _total = 0.0;
+
+  _openPaymentPage(context) {
+    Navigator.of(context).push(
+          MaterialPageRoute(builder: (BuildContext context) => Payment(total: _total,))
+    );
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    CartRepository.getCart().forEach((key, value) {
+      Product p = (value as Product);
+      _total += (p.productAmount * p.productPrice);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    print(CartRepository.getCart());
+    //initT();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Carrito'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => Profile()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(
+            flex: 75,
+            child: Container(
+              child: ListView.builder(
+                itemCount: CartRepository.getCart().length,
+                  itemBuilder: (BuildContext context, int index) {
+                    String key = CartRepository.getCart().keys.elementAt(index);
+                    Product product = CartRepository.getCart()[key];
+                    return ItemCart(
+                      product: product,
+                      add: (p) {
+                          setState(() {
+                            _total += p.productPrice;
+                          });
+                      },
+                      remove: (p) {
+                        setState(() {
+                          _total -= p.productPrice;
+                        });
+                      },
+                      onDeleteProduct: (amount) {
+                        setState(() {
+                          _total -= amount;
+                        });
+                      },
+                    );
+                  }
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 25,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Total', style: ThemeUtil.detailDescriptionTheme(),),
+                    Text(Formatter.currencyFormat(_total), style: ThemeUtil.detailPriceTheme(),)
+                  ],
+                ),
+                Container(
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  child: Padding(
+                    padding: EdgeInsets.all(
+                        16
+                    ),
+                    child: MaterialButton(
+                      height: 48,
+                      color: gray1,
+                      child: Text('PAGAR'),
+                      onPressed: () {
+                        _openPaymentPage(context);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )
+    );
   }
 }
--- practice_one/lib/cart/item_cart.dart
+++ practice_one/lib/cart/item_cart.dart
@@ -1,1 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:practice_one/colors.dart';
+import 'package:practice_one/models/cart_repository.dart';
+import 'package:practice_one/models/product.dart';
+import 'package:practice_one/utils/Formatter.dart';
+import 'package:practice_one/utils/favorite_widget.dart';
+import 'package:practice_one/utils/themes.dart';
 
+class ItemCart extends StatefulWidget {
+
+  final Product product;
+  final Function(Product p) add;
+  final Function(Product p) remove;
+  final Function(double amount) onDeleteProduct;
+
+  ItemCart({
+    Key key,
+    @required this.product,
+    this.add,
+    this.remove,
+    this.onDeleteProduct
+  }) : super(key: key);
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ItemCartState();
+  }
+}
+
+class _ItemCartState extends State<ItemCart> {
+
+  void _updateQty(int qty) {
+    setState(() {
+      widget.product.productAmount = qty;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Card(
+        elevation: 4.0,
+        margin: EdgeInsets.all(8.0),
+        color: orange2,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Image.network(widget.product.productImage, height: 150, fit:  BoxFit.fill,),
+            ),
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: EdgeInsets.only(left: 8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(widget.product.productTitle, style: ThemeUtil.itemCartTitle(),),
+                    Text(widget.product.productDescription, style: ThemeUtil.itemCartDescription(),),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.add_circle_outline),
+                            color: blue1,
+                          onPressed: (){
+                            _updateQty(widget.product.productAmount + 1);
+                            widget.add(widget.product);
+                          },
+                        ),
+                        Text('${widget.product.productAmount}', style: ThemeUtil.itemCartQty(),),
+                        IconButton(
+                          icon: Icon(Icons.remove_circle_outline),
+                          color: blue1,
+                          onPressed: (){
+                            if(widget.product.productAmount >= 1) {
+                              _updateQty(widget.product.productAmount - 1);
+                              widget.remove(widget.product);
+                            }
+                          },
+                        ),
+                        Text(Formatter.currencyFormat(widget.product.productPrice), style: ThemeUtil.itemCartPrice(),),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                FavIcon(
+                  isFavorite: widget.product.liked,
+                  callback: () {
+
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  color: blue1,
+                  onPressed: () {
+                    CartRepository.removeProduct(widget.product.productTitle);
+                    widget.onDeleteProduct(
+                      widget.product.productAmount * widget.product.productPrice
+                    );
+                  },
+                )
+              ],
+            )
+          ],
+      )
+    );
+  }
+}
