12a3fd2130d8dbbf57c7a5a039e86de81bb3e117
isaac <isaaccabrera31@gmail.com>
2020-02-16 12:39:17 -0600

    Create abstract class Product.

---
+++ practice_one/lib/models/product.dart
@@ -0,0 +1,9 @@
+
+abstract class Product {
+  String productTitle; // nombre del producto
+  String productDescription; // descripcion del producto
+  double productPrice; // precio del producto autocalculado
+  String productImage; // url de imagen del producto
+  bool liked;
+  int productAmount; // cantidad de producto por comprar
+}
