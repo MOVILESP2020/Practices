47e868fe64937c874f3606d1e0560f7f63bcdd54
isaac <isaaccabrera31@gmail.com>
2020-02-16 12:33:41 -0600

    Add colors.

---
+++ practice_one/lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const orange1 = const Color(0xFFEC9762);
+const orange2 = const Color(0xFFFABF7C);
+const gray1 = const Color(0xFFBCB0A1);
+const gray2 = const Color(0xFF8B8175);
+const blue1 = const Color(0xFF214254);
+const blue2 = const Color(0xFF121B22);
