f3b0accfbc731da17791d1742ed5d8765fd06971
isaac <isaaccabrera31@gmail.com>
2020-02-16 12:42:08 -0600

    Integrate navigation.

--- practice_one/lib/home/home.dart
+++ practice_one/lib/home/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:practice_one/drinks/drinks_page.dart';
+import 'package:practice_one/cart/cart.dart';
 import 'package:practice_one/home/item_home.dart';
-import 'package:practice_one/models/product_drinks.dart';
 import 'package:practice_one/models/product_repository.dart';
+import 'package:practice_one/products/cups/cups_page.dart';
+import 'package:practice_one/products/drinks/drinks_page.dart';
+import 'package:practice_one/products/grains/grains_page.dart';
 import 'package:practice_one/profile.dart';
 
 class Home extends StatefulWidget {
@@ -18,6 +20,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(widget.title),
         actions: <Widget>[
           IconButton(
@@ -30,7 +33,11 @@ class _HomeState extends State<Home> {
           ),
           IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => Cart()),
+              );
+            },
           )
         ],
       ),
@@ -43,27 +50,31 @@ class _HomeState extends State<Home> {
               image: "https://i.blogs.es/723857/cafe_como/450_1000.jpg",
             ),
           ),
-          ItemHome(
-            title: "Cafe en grano",
-            image: "https://www.elplural.com/uploads/s1/34/84/2/cafe.jpeg",
+          GestureDetector(
+            onTap: _openGrainsPage,
+            child: ItemHome(
+              title: "Cafe en grano",
+              image: "https://www.elplural.com/uploads/s1/34/84/2/cafe.jpeg",
+            ),
           ),
-          ItemHome(
-            title: "Tazas",
-            image:
-                "https://walkingmexico.com/wp-content/uploads/2015/02/Viajografi%CC%81a-Las-7-mejores-tazas-de-cafe%CC%81-en-el-D.F.-1.jpg",
-          ),
+          GestureDetector(
+            onTap: _openCupsPage,
+            child: ItemHome(
+              title: "Tazas",
+              image:
+              "https://walkingmexico.com/wp-content/uploads/2015/02/Viajografi%CC%81a-Las-7-mejores-tazas-de-cafe%CC%81-en-el-D.F.-1.jpg",
+            ),
+          )
         ],
       ),
     );
   }
 
   void _openDrinksPage() {
-    // TODO: completar en navigator pasando los parametros a la pagina de DrinksPage
-
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) {
-          return null;
+          return DrinksPage(drinksList: ProductRepository.loadProducts(ProductType.BEBIDAS),);
         },
       ),
     );
@@ -71,13 +82,17 @@ class _HomeState extends State<Home> {
 
   void _openGrainsPage() {
     Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) => null),
+      MaterialPageRoute(builder: (context) {
+        return GrainsPage(grainsList: ProductRepository.loadProducts(ProductType.GRANO),);
+      }),
     );
   }
 
   void _openCupsPage() {
     Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) => null),
+      MaterialPageRoute(builder: (context) {
+        return CupsPage(cupsList: ProductRepository.loadProducts(ProductType.TAZAS),);
+      }),
     );
   }
 }
