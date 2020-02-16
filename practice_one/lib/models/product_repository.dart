a83806d9238385269c3036b3b5e857265b6eddf3
isaac <isaaccabrera31@gmail.com>
2020-02-16 12:40:07 -0600

    Load cups impl.

--- practice_one/lib/models/product_repository.dart
+++ practice_one/lib/models/product_repository.dart
@@ -1,3 +1,4 @@
+import 'package:practice_one/models/product_cup.dart';
 import 'package:practice_one/models/product_drinks.dart';
 import 'package:practice_one/models/product_grains.dart';
 
@@ -6,9 +7,53 @@ enum ProductType { BEBIDAS, GRANO, TAZAS }
 class ProductRepository {
   static List<dynamic> loadProducts(ProductType prodType) {
     // ----------------------       Tazas      ----------------------------//
-    //if (prodType == ProductType.TAZAS) {
-    // TODO: Agregar al menos 5 productos "cup/taza"
-    //}
+    if (prodType == ProductType.TAZAS) {
+      return <ProductCups>[
+        ProductCups(
+          productTitle: "Lavazza",
+          productDescription: "Endulzado con aromas florales.",
+          productImage:
+          "https://images-na.ssl-images-amazon.com/images/I/61mH75J0TAL._SL1200_.jpg",
+          productColor: ProductColor.R,
+          productAmount: 0,
+        ),
+        ProductCups(
+          productTitle: "Guilis",
+          productDescription: "La mezcla de cinco orígenes diferentes.",
+          productImage:
+          "https://images-na.ssl-images-amazon.com/images/I/61mH75J0TAL._SL1200_.jpg",
+          productColor: ProductColor.R,
+          productAmount: 0,
+        ),
+        ProductCups(
+          productTitle: "Illy",
+          productDescription:
+          "Con toque de chocolate, miel, caramelo y un cierto amargor. ",
+          productImage:
+          "https://images-na.ssl-images-amazon.com/images/I/61mH75J0TAL._SL1200_.jpg",
+          productColor: ProductColor.R,
+          productAmount: 0,
+        ),
+        ProductCups(
+          productTitle: "Sanani",
+          productDescription:
+          "Tiene un muy buen sabor intenso y con fuerte acidez.",
+          productImage:
+          "https://images-na.ssl-images-amazon.com/images/I/61mH75J0TAL._SL1200_.jpg",
+          productColor: ProductColor.R,
+          productAmount: 0,
+        ),
+        ProductCups(
+          productTitle: "La Mexicana",
+          productDescription:
+          "Es un café único por la manera en la que están tostados sus granos.",
+          productImage:
+          "https://images-na.ssl-images-amazon.com/images/I/61mH75J0TAL._SL1200_.jpg",
+          productColor: ProductColor.R,
+          productAmount: 0,
+        ),
+      ];
+    }
 
     // ----------------------       Grano      ----------------------------//
     if (prodType == ProductType.GRANO)
@@ -25,7 +70,7 @@ class ProductRepository {
           productTitle: "Guilis",
           productDescription: "La mezcla de cinco orígenes diferentes.",
           productImage:
-              "https://www.cafesguilis.com/wp-content/uploads/2018/08/mezcla_especial_1Kg.jpg",
+          "https://www.lavazza.it/content/dam/lavazza/products/caffe/macinato/moka/qualitaoro/new_render/tin_250_en/Tin-oro-en-250-thumb.png",
           productWeight: ProductWeight.KILO,
           productAmount: 0,
         ),
@@ -34,7 +79,7 @@ class ProductRepository {
           productDescription:
               "Con toque de chocolate, miel, caramelo y un cierto amargor. ",
           productImage:
-              "https://http2.mlstatic.com/illy-cafe-en-grano-etiopia-arabica-seleccion-de-un-solo-or-D_NQ_NP_987783-MLM31233980209_062019-F.jpg",
+          "https://www.lavazza.it/content/dam/lavazza/products/caffe/macinato/moka/qualitaoro/new_render/tin_250_en/Tin-oro-en-250-thumb.png",
           productWeight: ProductWeight.KILO,
           productAmount: 0,
         ),
@@ -43,7 +88,7 @@ class ProductRepository {
           productDescription:
               "Tiene un muy buen sabor intenso y con fuerte acidez.",
           productImage:
-              "https://http2.mlstatic.com/cafe-tostado-en-granos-1kg-origen-colombia-maquinas-express-D_NQ_NP_947249-MLA31115265526_062019-Q.jpg",
+          "https://www.lavazza.it/content/dam/lavazza/products/caffe/macinato/moka/qualitaoro/new_render/tin_250_en/Tin-oro-en-250-thumb.png",
           productWeight: ProductWeight.KILO,
           productAmount: 0,
         ),
@@ -52,7 +97,7 @@ class ProductRepository {
           productDescription:
               "Es un café único por la manera en la que están tostados sus granos.",
           productImage:
-              "https://www.lamexicana.es/404-large_default/cafe-1890-mezcla-suave.jpg",
+          "https://www.lavazza.it/content/dam/lavazza/products/caffe/macinato/moka/qualitaoro/new_render/tin_250_en/Tin-oro-en-250-thumb.png",
           productWeight: ProductWeight.KILO,
           productAmount: 0,
         ),
